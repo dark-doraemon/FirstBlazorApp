a3ba1b51be2be9e4fe2b2cf83779feeb4a0806d3
Counter Logic <tuandinhbi75@gmail.com>
2024-05-24 12:20:49 +0700

    first push

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace FirstBlazorApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            //sử dụng Blazor cần phải có Razor pages
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();//Blazor
+
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapBlazorHub();
+
+            //First the Fallback Route which is _Host.cshtml is called.
+            app.MapFallbackToPage("/_Host");
+
+            app.Run();
+        }
+    }
+}
