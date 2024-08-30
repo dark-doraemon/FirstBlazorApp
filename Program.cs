0ad4c58d104453f0325dd3eceb62a4f7fb469bc7
dark-doraemon <tuandinhbi75@gmail.com>
2024-08-30 20:48:49 +0700

    30Aug2024-Create first project and some basic concepts

--- Program.cs
+++ Program.cs
@@ -5,14 +5,16 @@ namespace FirstBlazorApp
 {
     public class Program
     {
+        // “Blazor Server” configuration
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
-
-            //sử dụng Blazor cần phải có Razor pages
+            //Start by adding support for Razor Pages in the app since Blazor needs Razor Pages to work
             builder.Services.AddRazorPages();
-            builder.Services.AddServerSideBlazor();//Blazor
+
+            //add Server-Side Blazor services to the service collection
+            builder.Services.AddServerSideBlazor();
 
 
             var app = builder.Build();
@@ -26,9 +28,14 @@ namespace FirstBlazorApp
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseRouting();
+
+            //integrate Blazor with ASP.NET Core Endpoint Routing
+            //It allows app to call any Razor component from inside the ASP.NET Core Controller’s views or Razor pages.
+            //This will let SignalR which is the part of ASP.NET Core that handles the persistent HTTP request to work properly
             app.MapBlazorHub();
 
             //First the Fallback Route which is _Host.cshtml is called.
+            //Fallback Route là route dự phòng, nó có độ ưu tiên thấp, chỉ được gọi khi mà những route khác không match
             app.MapFallbackToPage("/_Host");
 
             app.Run();
